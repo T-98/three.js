8bede90e80b79519a8144551739bfe4bfe4607e5
T-98 Surface <dkhare@ucsc.edu>
2021-06-20 22:49:37 -0700

    added object and ambient light

--- js/script.js
+++ js/script.js
@@ -7,19 +7,38 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 //adding renderer to work with all DOM elements
 document.body.appendChild( renderer.domElement );
 
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
+//adding controls to pan and zoom
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+//adding object 
+var mtloader = new THREE.MTLLoader();
+mtloader.setPath("./assets/");
+mtloader.load("r2-d2.mtl", function(material){
+    material.preload();
+    var objloader = new THREE.OBJLoader();
+    objloader.setMaterials(material);
+    objloader.setPath("./assets/");
+    objloader.load("r2-d2.obj", function(object){
+        scene.add(object);
+        object.position.y-= 60;
+    });
+
+});
+/* SECTION REMOVED TO DEMO 3D OBJECTS LOADING
 //setting up geometry to include shapes
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-const controls = new THREE.OrbitControls( camera, renderer.domElement );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
+*/
 
 camera.position.z = 5;
 
 function animate() {
     requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     renderer.render( scene, camera );
 }
 animate();
