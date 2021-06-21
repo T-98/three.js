8b66a01223567ea6bcf11a229c4d6e6b1eaacf23
T-98 Surface <dkhare@ucsc.edu>
2021-06-20 22:02:10 -0700

    added orbit controls

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//setting up the scene with camera and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+//adding renderer to work with all DOM elements
+document.body.appendChild( renderer.domElement );
+
+//setting up geometry to include shapes
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
+animate();
